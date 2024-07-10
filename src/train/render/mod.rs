952c5a58089ed040ba0941d57020335f85684d4c
Patrick Lerner <ptlerner@gmail.com>
2024-07-10 23:31:47 +0200

    Remove jitter of train and remove shine of models (#63)

--- src/train/render/mod.rs
+++ src/train/render/mod.rs
@@ -4,6 +4,7 @@ mod tests;
 mod move_train_component;
 mod spawn_train_component;
 
+use crate::moving_things;
 use bevy::prelude::*;
 
 pub struct TrainRenderPlugin;
@@ -12,7 +13,7 @@ impl Plugin for TrainRenderPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (spawn_train_component::system, move_train_component::system),
+            (spawn_train_component::system, move_train_component::system).after(moving_things),
         );
     }
 }
