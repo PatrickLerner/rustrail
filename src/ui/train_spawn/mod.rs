7b2fbbe75d2863b82e8afdc0deb608c0542cfd22
Patrick Lerner <ptlerner@gmail.com>
2024-07-12 00:43:32 +0200

    Add BR 147/186

--- src/ui/train_spawn/mod.rs
+++ src/ui/train_spawn/mod.rs
@@ -107,6 +107,54 @@ fn spawn(mut commands: Commands, mut contexts: EguiContexts, mut id: Local<i64>)
                 });
             }
 
+            if ui.small_button("BR 147 (with passenger wagons)").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BR147.toml"))
+                    .insert(Name(format!("BR 147 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui.small_button("BR 186 (with passenger wagons)").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BR186.toml"))
+                    .insert(Name(format!("BR 186 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
             if ui.small_button("BR 52 (with passenger wagons)").clicked() {
                 *id += 1;
                 let engine = commands
