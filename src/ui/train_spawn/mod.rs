dde593d0bcb56117c93e0c3828290ca0104ad545
Patrick Lerner <ptlerner@gmail.com>
2024-07-12 20:40:51 +0200

    Add Baden VI c

--- src/ui/train_spawn/mod.rs
+++ src/ui/train_spawn/mod.rs
@@ -185,6 +185,33 @@ fn spawn(mut commands: Commands, mut contexts: EguiContexts, mut id: Local<i64>)
                     ..default()
                 });
             }
+
+            if ui
+                .small_button("Baden VI c (with passenger wagons)")
+                .clicked()
+            {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BadenVIc.toml"))
+                    .insert(Name(format!("BR 75 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
         },
     );
 }
