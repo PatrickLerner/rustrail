5b70a941538500363dd9b5da0ba63d95521b8a11
Patrick Lerner <ptlerner@gmail.com>
2024-07-13 00:49:39 +0200

    Add more models (#64)

--- src/ui/train_spawn/mod.rs
+++ src/ui/train_spawn/mod.rs
@@ -106,6 +106,189 @@ fn spawn(mut commands: Commands, mut contexts: EguiContexts, mut id: Local<i64>)
                     ..default()
                 });
             }
+
+            if ui.small_button("BR 147 (with passenger wagons)").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BR147.toml"))
+                    .insert(Name(format!("BR 147 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui.small_button("BR 186 (with passenger wagons)").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BR186.toml"))
+                    .insert(Name(format!("BR 186 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui.small_button("BR 52 (with passenger wagons)").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BR52.toml"))
+                    .insert(Name(format!("BR 52 {:0>3}", id.to_string())))
+                    .id();
+
+                let tender = commands
+                    .spawn(WagonBundle::from_file("assets/models/BR52_tender.toml"))
+                    .id();
+
+                let mut components = vec![
+                    TrainComponent::Engine(engine),
+                    TrainComponent::Wagon(tender),
+                ];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui
+                .small_button("Baden VI c (with passenger wagons)")
+                .clicked()
+            {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/BadenVIc.toml"))
+                    .insert(Name(format!("BR 75 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui
+                .small_button("SBB EE922 (with passenger wagons)")
+                .clicked()
+            {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/sbb_ee922.toml"))
+                    .insert(Name(format!("SBB EE922 {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui
+                .small_button("DGH 500 C (with passenger wagons)")
+                .clicked()
+            {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/DGH500C.toml"))
+                    .insert(Name(format!("Henschel DGH 500 C {:0>3}", id.to_string())))
+                    .id();
+
+                let mut components = vec![TrainComponent::Engine(engine)];
+
+                for _ in 0..3 {
+                    components.push(TrainComponent::Wagon(
+                        commands
+                            .spawn(WagonBundle::from_file("assets/models/nwagen.toml"))
+                            .id(),
+                    ));
+                }
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
+
+            if ui.small_button("VT98").clicked() {
+                *id += 1;
+                let engine = commands
+                    .spawn(EngineBundle::from_file("assets/models/VT98.toml"))
+                    .insert(Name(format!("VT 98 {:0>3}", id.to_string())))
+                    .id();
+
+                let control_car = commands
+                    .spawn(WagonBundle::from_file("assets/models/VS98.toml"))
+                    .id();
+
+                let components = vec![
+                    TrainComponent::Engine(engine),
+                    TrainComponent::Wagon(control_car),
+                ];
+
+                commands.spawn(TrainBundle {
+                    name: Name("RB 61".to_string()),
+                    composition: TrainComposition { components },
+                    ..default()
+                });
+            }
         },
     );
 }
