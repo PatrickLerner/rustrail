5b8ede8f63a60346b2cb2d3c16d328fa609ca8ff
Patrick Lerner <ptlerner@gmail.com>
2024-07-21 20:25:36 +0200

    fix test coverage

--- src/ui/load_scenario/tests.rs
+++ src/ui/load_scenario/tests.rs
@@ -1,2 +1,9 @@
-use coverage_helper::test;
 use super::*;
+use coverage_helper::test;
+
+#[test]
+fn plugin() {
+    let mut app = App::default();
+    app.add_plugins(LoadScenarioPlugin);
+    assert!(app.is_plugin_added::<LoadScenarioPlugin>());
+}
