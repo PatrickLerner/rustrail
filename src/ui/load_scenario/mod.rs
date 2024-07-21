511b4c11e883bacca004c044c50b56242055bde7
Patrick Lerner <ptlerner@gmail.com>
2024-07-21 20:14:02 +0200

    adjust ui

--- src/ui/load_scenario/mod.rs
+++ src/ui/load_scenario/mod.rs
@@ -11,23 +11,41 @@ fn load_scenario(
     mut contexts: EguiContexts,
     mut commands: Commands,
     mut window: Query<&mut Window, With<PrimaryWindow>>,
+    mut scenarios: Local<Option<Vec<(String, String)>>>,
 ) {
-    let files = glob("assets/scenarios/*.toml").unwrap();
+    if scenarios.is_none() {
+        let files = glob("assets/scenarios/*.toml").unwrap();
+        let items = files
+            .map(|file| {
+                let file_name = file.unwrap().display().to_string();
+                let scenario = ScenarioData::load_from_file(&file_name);
 
-    egui::Window::new("Load Scenario").show(contexts.ctx_mut(), |ui| {
-        for file in files {
-            let file_name = file.unwrap().display().to_string();
+                (file_name, scenario.info.name)
+            })
+            .collect();
 
-            if ui.small_button(&file_name).clicked() {
-                let scenario_data = ScenarioData::load_from_file(&file_name);
+        *scenarios = Some(items);
+    }
 
-                let mut window = window.single_mut();
-                window.title = format!("rustrail - {}", scenario_data.info.name);
+    let scenarios = scenarios.as_ref().unwrap();
 
-                commands.insert_resource(scenario_data);
+    egui::Window::new("Load Scenario")
+        .anchor(egui::Align2::CENTER_CENTER, (0.0, 0.0))
+        .collapsible(false)
+        .resizable(false)
+        .movable(false)
+        .show(contexts.ctx_mut(), |ui| {
+            for (file_name, scenario_name) in scenarios {
+                if ui.small_button(scenario_name).clicked() {
+                    let scenario_data = ScenarioData::load_from_file(&file_name);
+
+                    let mut window = window.single_mut();
+                    window.title = format!("rustrail - {}", scenario_data.info.name);
+
+                    commands.insert_resource(scenario_data);
+                }
             }
-        }
-    });
+        });
 }
 
 pub struct LoadScenarioPlugin;
